6fe64f6783a6eb35e0ed94db5f6e70bcb0b12999
gujarathijay <jaygujarathi1016@gmail.com>
2023-10-22 22:44:25 -0700

    GraphQL integration with MongoDB

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
-const app = express();
+const mongoose = require('mongoose');
+const Event = require('./models/event')
 
+const app = express();
 app.use(bodyParser.json());
-const events=[];
+
+
+// const events=[];
 app.use('/graphql',graphqlHttp({
     // "!" :- Non Nullable
     //events: [Event!]!  :- array of events that has type Event
@@ -42,23 +46,37 @@ app.use('/graphql',graphqlHttp({
     rootValue: {
         // queries and mutations should match resolver name
         events:()=>{
-
-            return events;
+            return Event.find().then(
+                events=>{
+                  return events.map(event =>{
+                    return {...event._doc}
+                  }) 
+                }
+            ).catch(err => {
+                console.error(err);
+                throw err;
+            })
         },
 
         // we can have args in queries as well for eg. events(args)
         //args hold all the variables of input type
         createEvent: (args) =>{            
-            const event  = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,  //+ is used to convert value to a number
-                date: args.eventInput.date
-
-            }
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date) //Convert into Date 
+            })
+            return event.save().then(
+                result=>{
+                  console.log(result);
+                  return {...result._doc}  
+                }
+            ).catch(err => {
+                console.error(err);
+                throw err;
+            })
+            // return event;
         }
     },
 
@@ -67,6 +85,13 @@ app.use('/graphql',graphqlHttp({
 
 }));
 
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@atlascluster.aillio9.mongodb.net/${process.env.MOMGO_DB}?retryWrites=true&w=majority`)
+.then(()=>{
+    app.listen(3000);
+    // console.log("here")
+})
+.catch(err =>{
+    console.log("Error connecting",err);
+});
 
 
-app.listen(3000);

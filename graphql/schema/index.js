fa45a961723d13315aaa77ff9d89c773880fd146
gujarathijay <jaygujarathi1016@gmail.com>
2023-11-07 15:12:39 -0800

    Added Bookings

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,45 +1,56 @@
- const {buildSchema} = require('graphql')
-    // "!" :- Non Nullable
-    //events: [Event!]!  :- array of events that has type Event
- module.exports = buildSchema(`
-        type Event{
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-            creator: User!
-        }
+const { buildSchema } = require('graphql');
 
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            createdEvents: [Event!]!
-        }
+module.exports = buildSchema(`
+type Booking {
+    _id: ID!
+    event: Event!
+    user: User!
+    createdAt: String!
+    updatedAt: String!
+}
 
-        input EventInput{
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
+type Event {
+  _id: ID!
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+  creator: User!
+}
 
-        input UserInput{
-            email: String!
-            password: String!
-        }
+type User {
+  _id: ID!
+  email: String!
+  password: String
+  createdEvents: [Event!]
+}
 
-        type RootQuery{
-            events : [Event!]!   
-        }
+input EventInput {
+  title: String!
+  description: String!
+  price: Float!
+  date: String!
+}
 
-        type RootMutation{
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-        schema{
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `)
+input UserInput {
+  email: String!
+  password: String!
+}
+
+type RootQuery {
+    events: [Event!]!
+    bookings: [Booking!]!
+}
+
+type RootMutation {
+    createEvent(eventInput: EventInput): Event
+    createUser(userInput: UserInput): User
+    bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
+}
+
+schema {
+    query: RootQuery
+    mutation: RootMutation
+}
+`);
